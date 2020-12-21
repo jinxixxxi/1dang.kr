ec7a462ecc7d075400557b2091a7fae777213dd6
jinxixxxi <75103524+jinxixxxi@users.noreply.github.com>
2020-12-21 18:50:20 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="ad.css">
+    <link media="only screen and (max-device-width: 480px)">
+    <link href="img/favicon.ico?1" rel="shortcut icon">
+    <title>일당이 필요할땐 1DANG.KR</title>
+</head>
+<body>
+        <div class="main-logo">
+            <br>
+            <br>
+            오늘 1DAY, 일당이 필요할땐
+                <br>
+                <a href="index.html"> <img src="img/logo.jpg" alt="1DANG.KR"> </a>
+                <br>
+        </div>
+        <div class="main-menu">
+                <li><a href="board.html"><b>서울</b></a></li> 
+                <li><a href="board.html"><b>경기</b></a></li>
+                <li><a href="board.html"><b>인천</b></a></li>
+                <li><a href="board.html"><b>부산</b></a></li>
+                <li><a href="board.html"><b>대구</b></a></li>
+                <li><a href="board.html"><b>대전</b></a></li>
+        </div>
+        <div class="main-menu">
+                <li><a href="board.html"><b>광주</b></a></li>
+                <li><a href="board.html"><b>세종</b></a></li>
+                <li><a href="board.html"><b>울산</b></a></li>
+                <li><a href="board.html"><b>강원</b></a></li>
+                <li><a href="board.html"><b>경남</b></a></li>
+                <li><a href="board.html"><b>경북</b></a></li>
+        </div>
+        <div class="main-menu">
+                <li><a href=""><b>전남</b></a></li>
+                <li><a href=""><b>전북</b></a></li>
+                <li><a href=""><b>충남</b></a></li>
+                <li><a href=""><b>충북</b></a></li>
+                <li><a href=""><b>제주</b></a></li>
+                <li><a href="" style="color:#FFAE18"><b>연변</b></a></li>
+        </div>
+        <div class="main-menu">
+                <li><a href=""><b>안산</b></a></li>
+                <li><a href="" style="color:#FFAE18"><b>시흥</b></a></li>
+                <li><a href=""><b>화성</b></a></li>
+                <li><a href=""><b>평택</b></a></li>
+                <li><a href=""><b>용인</b></a></li>
+                <li><a href=""><b>진천</b></a></li>
+        </div>
+        <div class="main-link">
+                <br>
+                <br>
+                <b>유료광고</b> 문의는 KakaoTalk ID: <b>1DANG</b>을 추가해주세요!
+                <br>
+                <br>
+                <a href="index.html"> <img src="img/SM.jpg">   </a>
+                <br>
+                <br>
+                <a href="index.html"> <img src="img/SM.jpg">   </a>
+                <br>
+                <br>
+                <a href="index.html"> <img src="img/SM.jpg">   </a>
+                <br>
+        </div>
+        <div class="main-bottom">
+          <br>
+            <br>
+                COPYRIGHT © <b>1DANG.KR</b> All Right Reserved.
+            <br>
+         <br>
+        </div>
+
+</body>
+</html>
