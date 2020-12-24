105e77d6bcd304e4f6fc24aebe7de1be82faca5b
jinxixxxi <75103524+jinxixxxi@users.noreply.github.com>
2020-12-24 15:37:27 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="Keywords" content="1DANG.KR, 일당, 알바, 구인, 구직, 취업">
+        <meta name="Description" content="1DNAG.KR, 일당, 알바, 구인, 구직, 취업">
+        <link rel="stylesheet" href="style.css">
+        <link media="only screen and (max-device-width: 480px)">
+        <link href="images/favicon.ico?1" rel="shortcut icon">
+        <title>일당이 필요할땐 1DANG.KR</title>
+    </head>
+    <body>
+        <div class="top">
+            <br>
+            <br>
+            오늘 1DAY, 일당이 필요할땐
+            <br>
+            <a href="index.html"> <img src="images/logo.jpg" alt="1DANG.KR"> </a>
+            <br>
+        </div>
+        <nav class="navbar">
+            <div class="index-channel">
+            <li><a href="board.html">서울</a></li> 
+            <li><a href="board.html">경기</a></li>
+            <li><a href="board.html">인천</a></li>
+            <li><a href="board.html">부산</a></li>
+            <li><a href="board.html">대구</a></li>
+            <li><a href="board.html">대전</a></li>
+            </div>
+            <div class="index-channel">
+            <li><a href="board.html">광주</a></li>
+            <li><a href="board.html">세종</a></li>
+            <li><a href="board.html">울산</a></li>
+            <li><a href="board.html">강원</a></li>
+            <li><a href="board.html">경남</a></li>
+            <li><a href="board.html">경북</a></li>
+            </div>
+            <div class="index-channel">
+            <li><a href="">전남</b></a></li>
+            <li><a href="">전북</b></a></li>
+            <li><a href="">충남</b></a></li>
+            <li><a href="">충북</b></a></li>
+            <li><a href="">제주</a></li>
+            <li><a href="" style="color:#FFAE18">연변</a></li>
+            </div>
+            <div class="index-channel">
+            <li><a href="" style="color:#FFAE18">시흥</a></li>
+            <li><a href="">안산</a></li>
+            <li><a href="">화성</a></li>
+            <li><a href="">평택</a></li>
+            <li><a href="">용인</a></li>
+            <li><a href="">진천</a></li>
+            </div>
+        </nav>
+        <div class="index-link">
+            <br>
+            <br>
+            <br>
+            <br>
+            <b>유료광고</b> 문의는 KakaoTalk ID: <b>1DANG</b>을 추가해주세요!
+            <br>
+            <br>
+            <a href="contact.html"> <img src="images/SM.jpg" alt="르노삼성 시화대리점 박미향 010-9328-8919"> </a>
+            <br>
+            <br>
+            <a href="index.html"> <img src="images/stanbuy.jpg" alt="광고 자리 임대 KakaoTalk ID: 1DANG"> </a>
+            <br>
+            <br>
+            <a href="index.html"> <img src="images/stanbuy.jpg" alt="광고 자리 임대 KakaoTalk ID: 1DANG"> </a>
+            <br>
+            </div>
+            <div class="bottom">
+            <br>
+            © Copyright <a href="http://www.1dang.kr"><b>1dang.kr</b></a>. All right reserved.
+            <br>
+            <br>
+        </div>
+    </body>
+</html>
